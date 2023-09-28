71d8d4ce13f8b8312d080afe87438b43fc9536a6
Victor Toledo <vito.toledo.a@gmail.com>
2023-09-27 22:03:14 -0300

    actualizado el diseño de product card

--- src/assets/components/ProductCard/index.tsx
+++ src/assets/components/ProductCard/index.tsx
@@ -12,16 +12,20 @@ export function ProductCard (product: ProductsInterface) {
 
   return(
     <div className="card-products">
-      <span className='card-products--like'>❤️</span>
-      <img className='card-products--img' src={product.rutaImg} alt="" />
-      <h3 className='card-products--title'>
-        <Link to={'/detalle'}>{product.title}</Link>
-      </h3>
-      <p className='card-products--desc'>{product.description}</p>
-      <section className='card-products--shopInfo'>
-        <p className="card-products--price">{'$ '+ product.price}</p>
-        <button className='card-products--addToCart'>Añadir al Carrito</button>
-      </section>
+      <div className="card-top">
+        <h3 className='card-title'>
+          <Link className='card-title--link' to={'/detalle'}>{product.title}</Link>
+        </h3>
+        <span className='like'>❤️</span>
+      </div>
+      <div className="card-middle">
+        <img className='card-img' src={product.rutaImg} alt="" />
+        <button className='addToCart'>Añadir al Carrito</button>
+      </div>
+      <div className="card-bottom">
+        <p className="price">{'$ '+ product.price}</p>
+        <p className='desc'>{product.description}</p>
+      </div>
     </div>
   )
 }
